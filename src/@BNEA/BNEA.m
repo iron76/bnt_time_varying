e7d886f60e14cc71bf0809860ba19cf2018eb96c
Francesco <francesco.nori@iit.it>
2015-01-09 15:43:33 +0100

    BNEA Added a string to represent the inference engine. Also fixed a bug on the initial dimensions of Sd.

--- src/@BNEA/BNEA.m
+++ src/@BNEA/BNEA.m
@@ -16,7 +16,7 @@
 classdef BNEA < stochasticIDsolver
    
    properties
-      bnt, evd, covPriorWeight;
+      bnt, evd, covPriorWeight, inf_engine;
    end
    methods
       function b = BNEA(m,y)
@@ -30,12 +30,21 @@ classdef BNEA < stochasticIDsolver
             end
          end
          b     = b@stochasticIDsolver(m,y);
-         b.Sd  = zeros(b.IDmodel.modelParams.NB, b.IDmodel.modelParams.NB);
+         b.Sd  = zeros(b.IDmodel.modelParams.NB*26, b.IDmodel.modelParams.NB*26);
          b.bnt = b.initNetFromModel(b.IDmodel.modelParams, b.IDsens.sensorsParams);
          b.evd = cell(1,6*b.IDmodel.modelParams.NB+b.IDsens.sensorsParams.ny);
          
          b.covPriorWeight = 1e-5;
+         b.inf_engine     = 'not_defined';
       end % BNEA
+      
+      function b = setEngine(b, str)
+         if strcmp(str, 'gaussian_inf_engine') || strcmp(str, 'jtree_inf_engine')
+            b.inf_engine = str;
+         else
+            disp('Not a valid inf_engine')
+         end
+      end % disp
       
       function disp(b)
          % Display BNEA object
@@ -44,25 +53,34 @@ classdef BNEA < stochasticIDsolver
       end % disp
       
       function b = setY(b,y)
+         if strcmp(b.inf_engine, 'not_defined')
+            error('You should defined a inf_engine with myBNEA.setEngine() before using the BNEA class')
+         end
          b = setY@deterministicIDsolver(b,y);
          b = updateEvd(b);
       end
-
+      
       function [y, yc] = simY(b)
+         if strcmp(b.inf_engine, 'not_defined')
+            error('You should defined a inf_engine with myBNEA.setEngine() before using the BNEA class')
+         end
          ys = sample_bnet(b.bnt.bnet);
          yc = cell(size(ys));
          y  = zeros(b.IDsens.sensorsParams.ny,1);
          NB = b.IDmodel.modelParams.NB;
-
+         
          j  = 1;
          for i = 1 : b.IDsens.sensorsParams.ny
             y(j:j+b.bnt.nodes.sizes{NB+i}-1) = ys{b.bnt.nodes.index{NB+i}};
             yc{b.bnt.nodes.index{NB+i}} = ys{b.bnt.nodes.index{NB+i}};
             j = j+b.bnt.nodes.sizes{NB+i};
          end
-      end      
+      end
       
       function b = setState(b,q,dq)
+         if strcmp(b.inf_engine, 'not_defined')
+            error('You should defined a inf_engine with myBNEA.setEngine() before using the BNEA class')
+         end
          b = setState@deterministicIDsolver(b,q,dq);
          b = updateNet(b);
       end
