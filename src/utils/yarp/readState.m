bcd9d72194573f75fdd81b5df91d6fa67568a99a
Francesco <francesco.nori@iit.it>
2015-01-20 02:03:43 +0100

    readState.m checks for multiple points at the same instant

--- src/utils/yarp/readState.m
+++ src/utils/yarp/readState.m
@@ -15,10 +15,16 @@ for j = 1 : 10
    format = [format, ') '];
 end
 C    = textscan(fid, format);
-time = (C{1, 2});
+time = C{1, 2};
 q    = cell2mat(C(1, 3    :3+  n-1));
 dq   = cell2mat(C(1, 3+  n:3+2*n-1));
 d2q  = cell2mat(C(1, 3+2*n:3+3*n-1));
+
+[tu,iu] = unique(time);
+time    = tu';
+q       = q(iu, :)';
+dq      = dq(iu, :)';
+d2q     = d2q(iu, :)';
 
 if fclose(fid) == -1
    error('[ERROR] there was a problem in closing the file')
