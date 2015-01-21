d3156dad65a06f56e6db42c4099deb6d7d9ae695
Silvio Traversaro <silvio.traversaro@iit.it>
2015-01-21 19:33:56 +0100

    Modified icub model, and started to rectify the sensors

--- experiments/main.m
+++ experiments/main.m
@@ -12,6 +12,7 @@ if ~exist('preprocess.mat', 'file')
 
    %%strucutre from files
    data.path        = '/Users/iron/Desktop/iron/myTex/2015-01-rss/data';
+%    data.path        = '/home/pegua/Documents/Papers/2015-01-rss/data';
    data.parts       = {'inertial'                                 , 'left_arm_accelerometers', 'left_foot_inertial'        , 'left_hand_inertial'                  , 'right_arm_accelerometers', 'right_foot_inertial'       , 'right_hand_inertial'                 , 'torso_accelerometers'                    , 'l_arm_ft_sensor:o', 'r_arm_ft_sensor:o', 'l_leg_ft_sensor:o', 'r_leg_ft_sensor:o', 'l_foot_ft_sensor:o'        , 'r_foot_ft_sensor:o'        , 'head'      , 'left_arm'  , 'right_arm' , 'left_leg'  , 'right_leg' , 'torso'     };
    data.labels      = {'imu'                                      , 'la_acc'                 , 'lf_acc'                    , 'lh_imu'                              , 'ra_acc'                  , 'rf_acc'                    , 'rh_imu'                              , 'to_acc'                                  , 'la_fts'           , 'ra_fts'           , 'll_fts'           , 'rl_fts'           , 'lf_fts'                    , 'rf_fts'                    , 'h'         , 'la'        , 'ra'        , 'll'        , 'rl'        , 'to'        };
    data.ndof        = {12                                         , 18                       , 3                           , 6                                     , 18                        , 3                           , 6                                     , 12                                        , 6                  , 6                  , 6                  , 6                  , 6                           , 6                           ,  6          , 16          , 16          , 6           , 6           , 3           };
@@ -25,6 +26,9 @@ if ~exist('preprocess.mat', 'file')
    sens.ndof        = {6                                          , 3                        , 3                           , 6                                     , 3                         , 3                           , 6                                     , 3                                         , 6                  , 6                  , 6                  , 6                  , 6                           , 6                           };
    sens.type        = {''                                         , 'analog:o'               , 'analog:o'                  , 'analog:o'                            , 'analog:o'                , 'analog:o'                  , 'analog:o'                            , 'analog:o'                                , ''                 , ''                 , ''                 , ''                 , ''                          , ''                          };
 
+   %%structure from sensors 
+   sens.transform   = {'X_chest_imu'                              , 'X_l_upper_arm_la_acc'   , 'X_l_upper_foot_lf_acc'     , 'X_l_forearm_lh_imu'                  , 'X_r_upper_arm_ra_acc'    , 'X_r_upper_foot_rf_acc'     , 'X_r_forearm_rh_imu'                  , 'X_chest_to_acc'                          , 'X_l_upper_arm_la_fts_force','X_r_upper_arm_ra_fts_force','X_l_thigh_ll_fts_force','X_r_thigh_rl_fts_force','X_l_upper_foot_lf_fts_force'     , 'X_r_upper_foot_rf_fts_force'     }; 
+   
    %%
    % for i = 1 : length(data.time)
    %    iCubVisualize(data.q(:,i), R)
@@ -84,8 +88,86 @@ py = [0; cumsum(cell2mat(myPNEA.IDsens.sensorsParams.sizes))];
 %       title(strrep(myPNEA.IDsens.sensorsParams.labels{i}, '_', '~'));
 %    end
 % end
+sens.transform   = {'X_chest_imu'                              , 'X_l_upper_arm_la_acc'   , 'X_l_upper_foot_lf_acc'     , 'X_l_forearm_lh_imu'                  , 'X_r_upper_arm_ra_acc'    , 'X_r_upper_foot_rf_acc'     , 'X_r_forearm_rh_imu'                  , 'X_chest_to_acc'                          , 'X_l_upper_arm_la_fts_force','X_r_upper_arm_ra_fts_force','X_l_thigh_ll_fts_force','X_r_thigh_rl_fts_force','X_l_upper_foot_lf_fts_force'     , 'X_r_upper_foot_rf_fts_force'     }; 
+%label_to_plot = {'imu', 'la_acc', 'lf_acc', 'lh_imu', 'ra_acc', 'rf_acc', 'rh_imu', 'to_acc', 'la_fts', 'ra_fts', 'll_fts', 'rl_fts', 'lf_fts', 'rf_fts'};
+label_to_plot  = {'lf_fts', 'rf_fts'};
 
-label_to_plot = {'imu', 'la_acc', 'lf_acc', 'lh_imu', 'ra_acc', 'rf_acc', 'rh_imu', 'to_acc', 'la_fts', 'ra_fts', 'll_fts', 'rl_fts', 'lf_fts', 'rf_fts'};
+sensorFrameExtraction
+
+%% Process raw sensor data and bring it in the desired reference frames
+acc_gain = 5.9855e-04;
+deg_to_rad = pi/180.0;
+gyro_gain = deg_to_rad*7.6274e-03;
+for l = 1 : length(label_to_plot)
+   for i = 1 : length(data.parts)
+      if strcmp(data.labels{i}, label_to_plot{l})
+         t    = ['time_' data.labels{i}];
+         ys   = ['ys_' data.labels{i}];
+         J = length(eval(data.index{i}));
+         if( strcmp(data.labels{i},'lh_imu') || ...
+             strcmp(data.labels{i},'rh_imu') )
+            eval(['data.ys_' data.labels{i} '(1:3,:) = ' ...
+                  'acc_gain*data.ys_' data.labels{i} '(1:3,:)']);
+            eval(['data.ys_' data.labels{i} '(4:6,:) = ' ...
+                  'gyro_gain*data.ys_' data.labels{i} '(4:6,:)']);
+         end
+          if( strcmp(data.labels{i}(end-2:end),'acc') )
+             eval(['data.ys_' data.labels{i} '(1:3,:) = ' ...
+                   'acc_gain*data.ys_' data.labels{i} '(1:3,:)']);
+             eval(['data.ys_' data.labels{i} ' = ' ...
+                    sens.transform{i} '(1:3,1:3) * ' 'data.ys_' data.labels{i}]);
+          end
+         if( strcmp(data.labels{i}(end-2:end),'imu') )
+             eval(['data.ys_' data.labels{i} ' = ' ...
+                   sens.transform{i} ' * ' 'data.ys_' data.labels{i}]);
+         end
+         % account for the wrong offset present in the input data
+         if( strcmp(data.labels{i}(end-2:end),'fts') )
+             eval(['data.ys_' data.labels{i} ' = ' ...
+                    'data.ys_' data.labels{i}]);
+         end
+         if( strcmp(data.labels{i}(end-2:end),'fts') )
+             eval(['data.ys_' data.labels{i} ' = ' ...
+                   sens.transform{i} ' * ' 'data.ys_' data.labels{i}]);
+         end
+      end
+   end
+end
+
+%% Plot overlapped plots
+% for l = 1 : length(label_to_plot)
+%    for i = 1 : length(data.parts)
+%       if strcmp(data.labels{i}, label_to_plot{l})
+%          t    = ['time_' data.labels{i}];
+%          ys   = ['ys_' data.labels{i}];
+%          
+%          figure
+%          J = length(eval(data.index{i}));
+%          for j = 1 : J/3
+%             subplot([num2str(J/3) '1' num2str(j)])
+%             I = 1+(j-1)*3 : 3*j;
+%             eval(['plot(data.time,data.' ys '(I,:), ''--'' )' ]);
+%             hold on;
+%             title(strrep(['y_{' data.labels{i} '}'], '_', '~'))
+%          end
+%       end
+%    end
+%    for k = 1 : myPNEA.IDsens.sensorsParams.ny - dmodel.NB
+%       if strcmp(myPNEA.IDsens.sensorsParams.labels{k}, label_to_plot{l})
+%          %figure
+%          J = myPNEA.IDsens.sensorsParams.sizes{k};
+%          for j = 1 : J/3
+%             subplot([num2str(J/3) '1' num2str(j)])
+%             I = py(k)+1+(j-1)*3 : py(k)+3*j;
+%             plot(data.time, y(I,:))
+%             title(strrep(myPNEA.IDsens.sensorsParams.labels{k}, '_', '~'));
+%          end
+%       end
+%    end
+%    
+% end
+
+%% Plot separated graphs
 for l = 1 : length(label_to_plot)
    for i = 1 : length(data.parts)
       if strcmp(data.labels{i}, label_to_plot{l})
