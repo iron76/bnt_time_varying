00dfe948c8b05e8707dbb18de84d2d6cde533948
Francesco <francesco.nori@iit.it>
2015-01-21 20:46:51 +0100

    modified for the learning

---
+++ experiments/learnBNEAicub.m
@@ -0,0 +1,61 @@
+clear all
+close all
+load preprocess.mat
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+myBNEA  = BNEA(myModel, mySens);
+myBNEA  = myBNEA.setEngine('jtree_inf_engine');
+NB      = myModel.modelParams.NB;
+[~, n]  = size(y);
+
+bnet    = cell(1,n);
+engine  = cell(1,n);
+sample  = cell(NB*6+myBNEA.IDsens.sensorsParams.ny,n);
+
+for i = 1 : n
+  
+   myBNEA = myBNEA.setState(data.q(:,i), data.dq(:,i));
+   myBNEA = myBNEA.setY(y(:,i));
+   
+   bnet{1,i}   = myBNEA.bnt.bnet;
+   engine{1,i} = myBNEA.bnt.engine;
+   sample(:,i) = myBNEA.evd';
+end
+
+i_learn = [];
+for i = 1 : length(myBNEA.bnt.nodes.labels)
+   if strcmp(myBNEA.bnt.nodes.labels{i}(1:2), 'y_') && ~strcmp(myBNEA.bnt.nodes.labels{i}(end-2:end), 'ftx')
+      i_learn = [i_learn, i];
+   end
+end
+
+% This parameters should be kept small (1e-15)for
+% simulated data. For real data is plays the role
+% of a regularization factor and should be tuned so
+% as to have non-decresing EM steps.
+cov_prior_weight = 1e-3;
+bnetHat = EM_bnet_learn(bnet, sample, cov_prior_weight, i_learn);
+
+dir_ind = cell2mat(myBNEA.bnt.nodes.index);
+inv_ind(dir_ind) = 1:length(dir_ind);
+
+for i = 1 : length(dir_ind(1:NB*6))
+   cov_ini = get_field(myBNEA.bnt.bnet.CPD{dir_ind(i)}, 'cov');
+   cov_est = get_field(bnetHat.CPD{dir_ind(i)},         'cov');
+   cov_upd = cov_est - cov_ini;
+   if cov_upd
+      fprintf('[ERROR] Something wrong with clamped covariance! \n')
+      return;
+   end
+end
+
+% Depending on the number of samples 'n' the updates can
+% be quite relevant. With increasing 'n' the magnitude of
+% the updates tends to become smaller.
+
+for i = length(dir_ind(1:NB*6))+1 : length(dir_ind)
+   cov_ini = get_field(myBNEA.bnt.bnet.CPD{dir_ind(i)}, 'cov');
+   cov_est = get_field(bnetHat.CPD{dir_ind(i)},         'cov');
+   cov_upd = cov_est - cov_ini;
+   fprintf('[INFO] %s was updated by %f \n', myBNEA.bnt.nodes.labels{i}, norm(cov_upd)./norm(cov_ini));
+end
