b7aa6468906b58b9f6c8df40296d63fa1ddaacd2
Francesco <francesco.nori@iit.it>
2015-01-22 11:48:40 +0100

    error bar plots improved

--- experiments/predictSNEAicub.m
+++ experiments/predictSNEAicub.m
@@ -66,38 +66,23 @@ end
 py = [0; cumsum(cell2mat(myPNEA.IDsens.sensorsParams.sizes))];
 label_to_plot = {'imu', 'la_acc', 'lf_acc', 'lh_imu', 'ra_acc', 'rf_acc', 'rh_imu', 'to_acc', 'la_fts', 'ra_fts', 'll_fts', 'rl_fts', 'lf_fts', 'rf_fts'};
 for l = 1 : length(label_to_plot)
-   for i = 1 : length(data.parts)
-      if strcmp(data.labels{i}, label_to_plot{l})
-         t    = ['time_' data.labels{i}];
-         ys   = ['ys_' data.labels{i}];
-         
-         figure
-         J = length(eval(data.index{i}));
-         for j = 1 : J/3
-            subplot([num2str(J/3) '1' num2str(j)])
-            I = 1+(j-1)*3 : 3*j;
-            eval(['plot(data.time,data.' ys '(I(1),:), ''r'' )' ]);
-            eval(['plot(data.time,data.' ys '(I(2),:), ''g'' )' ]);
-            eval(['plot(data.time,data.' ys '(I(3),:), ''b'' )' ]);
-            hold on;
-            title(strrep(['y_{' data.labels{i} '}'], '_', '~'))
-         end
-      end
-   end
    for k = 1 : myPNEA.IDsens.sensorsParams.ny - dmodel.NB
       if strcmp(myPNEA.IDsens.sensorsParams.labels{k}, label_to_plot{l})
-         %figure
+         figure
          J = myPNEA.IDsens.sensorsParams.sizes{k};
          for j = 1 : J/3
             subplot([num2str(J/3) '1' num2str(j)])
             hold on;
             I = py(k)+1+(j-1)*3 : py(k)+3*j;
-            shadedErrorBar(data.time, res.y(I(1),:), sqrt(reshape(res.Sy(I(1), I(1), :), 1, n)), {'--r' , 'LineWidth', 1}, 0);
-            shadedErrorBar(data.time, res.y(I(2),:), sqrt(reshape(res.Sy(I(2), I(2), :), 1, n)), {'--g' , 'LineWidth', 1}, 0);
-            shadedErrorBar(data.time, res.y(I(3),:), sqrt(reshape(res.Sy(I(3), I(3), :), 1, n)), {'--b' , 'LineWidth', 1}, 0);
+            shadedErrorBar(data.time, data.y(I(1),:), sqrt(data.Sy(I(1), :)), {'--r' , 'LineWidth', 1}, 0);
+            shadedErrorBar(data.time, data.y(I(2),:), sqrt(data.Sy(I(2), :)), {'--g' , 'LineWidth', 1}, 0);
+            shadedErrorBar(data.time, data.y(I(3),:), sqrt(data.Sy(I(3), :)), {'--b' , 'LineWidth', 1}, 0);
+            
+            shadedErrorBar(data.time,  res.y(I(1),:), sqrt(res.Sy(I(1), :)), {'r' , 'LineWidth', 1}, 0);
+            shadedErrorBar(data.time,  res.y(I(2),:), sqrt(res.Sy(I(2), :)), {'g' , 'LineWidth', 1}, 0);
+            shadedErrorBar(data.time,  res.y(I(3),:), sqrt(res.Sy(I(3), :)), {'b' , 'LineWidth', 1}, 0);
             title(strrep(myPNEA.IDsens.sensorsParams.labels{k}, '_', '~'));
          end
       end
    end
-   
 end
