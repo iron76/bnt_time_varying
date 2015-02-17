1278975b98e171743eda10332fa31580c1d328eb
Francesco <francesco.nori@iit.it>
2015-02-17 17:39:43 +0100

    Inserted (but commented) the derivatives of the D function

--- tests/testDerivatives.m
+++ tests/testDerivatives.m
@@ -5,7 +5,7 @@ clc
 joint_to_test = {'Rx', 'Ry', 'Rz', 'Px', 'Py', 'Pz', 'R', 'P'};
 % joint_to_test = {'Ry'};
 
-num_of_tests  = 1000;
+num_of_tests  = 10;
 
 for i = 1 : length(joint_to_test)
    for j = 1 : num_of_tests
@@ -21,3 +21,26 @@ for i = 1 : length(joint_to_test)
       end
    end
 end
+
+% NB      = 30;
+% dmodel  = autoTree(NB);
+% ymodel  = autoSensSNEA(dmodel);
+% 
+% dmodel  = autoTreeStochastic(dmodel);
+% ymodel  = autoSensStochastic(ymodel);
+% 
+% myModel = model(dmodel);
+% mySens  = sensors(ymodel);
+% mySNEA  = SNEA(myModel, mySens);
+% 
+% for i = 1 : num_of_tests
+%    q  = rand(dmodel.NB     ,1);
+%    dq = rand(dmodel.NB     ,1);
+%    d  = rand(dmodel.NB * 26,1);
+%    f  = @(x) computeDb(mySNEA , d , x);
+%    % D  = computeD(mySNEA , [q, dq]);
+%    dD = deriv(f, [q; dq]);  
+% end
+
+
+
