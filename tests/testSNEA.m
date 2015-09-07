b55cd9639569c415734f65c501cddf058c7d4987
Francesco <francesco.nori@iit.it>
2015-09-07 14:30:59 +0200

    SNEA and RNEA are now compared on the same set of sensors and with indetities in the covariance matrices

--- tests/testSNEA.m
+++ tests/testSNEA.m
@@ -1,23 +1,32 @@
-function res = testSNEA(dmodel, ymodel)
+function res = testSNEA(dmodel)
 
 res = 0;
 
+ymodel_RNEA  = autoSensRNEA(dmodel);
+ymodel_SNEA  = autoSensStochastic( ymodel_RNEA );
+mySens_SNEA  = sensors( ymodel_SNEA );
+
+dmodel_SNEA  = autoTreeStochastic(dmodel);
+% set Sw_inv to zero
+i = dmodel_SNEA.Sw_inv.i;
+j = dmodel_SNEA.Sw_inv.j;
+for k = 1:length(i)
+   dmodel_SNEA.Sw_inv = set(dmodel_SNEA.Sw_inv, zeros(size(dmodel_SNEA.Sw_inv(i(k),j(k)))), i(k), j(k));
+end
+myModel_SNEA = model(dmodel_SNEA);
+
 q         = rand(dmodel.NB,1);
 dq        = rand(dmodel.NB,1);
-y         = rand(ymodel.m,1);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-myModel = model(dmodel);
-mySens  = sensors(ymodel);
+y         = rand(ymodel_RNEA.m,1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-myRNEA    = RNEA(myModel, mySens);
+myRNEA    = RNEA(myModel_SNEA, mySens_SNEA);
 myRNEA    = myRNEA.setState(q,dq);
 myRNEA    = myRNEA.setY(y);
 myRNEA    = myRNEA.solveID();
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-mySNEA    = SNEA(myModel, mySens);
+mySNEA    = SNEA(myModel_SNEA, mySens_SNEA);
 mySNEA    = mySNEA.setState(q,dq);
 mySNEA    = mySNEA.setY(y);
 mySNEA    = mySNEA.solveID();
@@ -39,7 +48,7 @@ end
 
 
 disp(['[SNEA] Diff between RNEA and SNEA is ' num2str(norm(mySNEA.d-myRNEA.d))]);
-if norm(mySNEA.d-myRNEA.d) > 2.0
+if norm(mySNEA.d-myRNEA.d) > 1e-3
    disp('[SNEA] Result is excessively inaccurate. Test is declared failed!');
    res = 1;
 end   
