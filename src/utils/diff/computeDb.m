6b85102ad5ccfa506b743a6f3e35e7dd9dab7f19
Francesco <francesco.nori@iit.it>
2015-09-11 19:25:24 +0200

    modified for the sparse version

--- src/utils/diff/computeDb.m
+++ src/utils/diff/computeDb.m
@@ -9,9 +9,14 @@ NB = m/2;
 q  = x(1:m/2,1);
 dq = x(m/2+1:m,1);
 myNEA  = myNEA.setState(q, dq);
-D = sparse(myNEA.iDs, myNEA.jDs, myNEA.Ds, 19*NB, 26*NB);
-b = sparse(myNEA.ibs, ones(size(myNEA.ibs)), myNEA.bs, 19*NB, 1);
-D = D(:, myNEA.id);
+if ~isa(myNEA, 'ANEA') && ~isa(myNEA, 'DANEA') 
+   D = sparse(myNEA.iDs, myNEA.jDs, myNEA.Ds, 19*NB, 26*NB);
+   b = sparse(myNEA.ibs, ones(size(myNEA.ibs)), myNEA.bs, 19*NB, 1);
+   D = D(:, myNEA.id);
+else
+   D = myNEA.D.matrix;
+   b = myNEA.b.matrix;
+end
 
 Db = D * d + b;
 end
