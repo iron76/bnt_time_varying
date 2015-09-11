d1bf18254aea3fd1b810aa7edb5b40ef651f14c8
Francesco <francesco.nori@iit.it>
2015-09-11 17:15:14 +0200

    sparse matrix is now used

--- src/utils/diff/computeDb.m
+++ src/utils/diff/computeDb.m
@@ -4,12 +4,15 @@ function [ Db ] = computeDb( myNEA , d , x )
 if n ~= 1
    error('In computing D(x), x should be a column vector')
 end
+NB = m/2;
 
 q  = x(1:m/2,1);
 dq = x(m/2+1:m,1);
 myNEA  = myNEA.setState(q, dq);
-D = myNEA.D.matrix;
-b = myNEA.b.matrix;
+D = sparse(myNEA.iDs, myNEA.jDs, myNEA.Ds, 19*NB, 26*NB);
+b = sparse(myNEA.ibs, ones(size(myNEA.ibs)), myNEA.bs, 19*NB, 1);
+D = D(:, myNEA.id);
+
 Db = D * d + b;
 end
 
