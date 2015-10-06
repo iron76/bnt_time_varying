ec9fd13c05d2e450293bef8ae8a39b1eb0c9b3c6
Francesco <francesco.nori@iit.it>
2015-10-06 16:19:18 +0900

    added the functions for plotting labels in the covariance matrices

--- experiments/icubFixedBase/ADNEA/2015-10-minVariance/main.m
+++ experiments/icubFixedBase/ADNEA/2015-10-minVariance/main.m
@@ -1,4 +1,5 @@
 close all
+clear all
 NB        = 2;
 nsamples  = 1000;
 
@@ -30,59 +31,37 @@ K      = myMAP.id;
 Iinv(I)= 1:length(I);
 Jinv(J)= 1:length(J);
 Kinv(K)= 1:length(K);
+P      = Kinv(J);
+Pinv(P)= 1:length(P);
 
-iIndex  = [1 cumsum(myMAP.iSizes)+1];
 iLabels = myMAP.iLabels;
-jIndex  = [1 cumsum(myMAP.jSizes)+1];
 jLabels = myMAP.jLabels;
+iIndex = myMAP.iIndex;
+jIndex = myMAP.jIndex;
 
-
-S    = Sd1(Kinv, Kinv);
 figure
-imagesc(Sd1(Kinv(J), Kinv(J))) % Sd1(Kinv(J), Kinv(J)) == S(J,J)
+imagesc(Sd1(P, P))
 colorbar
 rangeS = caxis;
-set(gca, 'XTick', [], 'XTickLabel', []);
-set(gca, 'YTick', [], 'YTickLabel', []);
-setLabels('fx',  0*NB+1 : 6 :  6*NB, 'X')
-setLabels('fx',  0*NB+1 : 6 :  6*NB, 'Y')
-setLabels('q',   6*NB+1            , 'X')
-setLabels('q',   6*NB+1            , 'Y')
-setLabels('a',   7*NB+1 : 6 : 13*NB, 'X')
-setLabels('a',   7*NB+1 : 6 : 13*NB, 'Y')
-setLabels('fB', 13*NB+1 : 6 : 19*NB, 'X')
-setLabels('fB', 13*NB+1 : 6 : 19*NB, 'Y')
-setLabels('f',  19*NB+1 : 6 : 25*NB, 'X', 1)
-setLabels('f',  19*NB+1 : 6 : 25*NB, 'Y', 1)
-setLabels('t',  25*NB+1            , 'X')
-setLabels('t',  25*NB+1            , 'Y')
+jIndex_new  = Pinv(jIndex);
+jLabels_new = jLabels(P);
+[~, Oj] = sort(jIndex_new);
+% iIndex_new  = Iinv(iIndex);
+% iLabels_new = iLabels(I);
+% [~, Oi] = sort(iIndex_new);
+set(gca, 'XTick', jIndex_new(Oj), 'XTickLabel', jLabels_new(jIndex_new(Oj)))
+set(gca, 'YTick', jIndex_new(Oj), 'YTickLabel', jLabels_new(jIndex_new(Oj)))
+title('Initial covariance matrix')
 
-% figure
-% imagesc(S(J, J))
-% colorbar
-% rangeS = caxis;
-% set(gca, 'XTick', jIndex, 'XTickLabel', jLabels{J})
+[~, jMax] = max(diag(abs(Sd1(Kinv, Kinv))));
 
-% figure
-% subplot(211)
-% imagesc(S([myMAP.jF(end:-1:1, 1)], [myMAP.jF(end:-1:1, 1)]))
-% colorbar
-% rangeF = caxis;
-% subplot(212)
-% imagesc(S([myMAP.ja], [myMAP.ja]))
-% colorbar
-% rangeA = caxis;
-
-dS   = diag(abs(S));
-[~, jMax] = max(dS);
-
-if ~isempty(find(jMax==myMAP.jF))
+if ~isempty(find(jMax==myMAP.jF,1))
    num = num2str(find(jMax==myMAP.jF));
    disp(['Worse estimation is on f' num])
    ymodel = addSens(ymodel, ['f' num], eye(6));
    mySens  = sensors( ymodel );
 end
-if ~isempty(find(jMax==myMAP.jtau))
+if ~isempty(find(jMax==myMAP.jtau,1))
    num = num2str(find(jMax==myMAP.jtau));
    disp(['Worse estimation is on tau' num]);
    ymodel = addSens(ymodel, ['tau' num], 1);
@@ -103,40 +82,29 @@ K      = myMAP.id;
 Iinv(I)= 1:length(I);
 Jinv(J)= 1:length(J);
 Kinv(K)= 1:length(K);
+P      = Kinv(J);
+Pinv(P)= 1:length(P);
 
-S    = Sd2(Kinv, Kinv);
 figure
-imagesc(Sd2(Kinv(J), Kinv(J))) % Sd2(Kinv(J), Kinv(J)) == S(J,J)
+imagesc(Sd2(P, P)) % Sd2(Kinv(J), Kinv(J)) == S(J,J)
 colorbar
 caxis(rangeS);
+jIndex_new  = Pinv(jIndex);
+jLabels_new = jLabels(P);
+[~, Oj] = sort(jIndex_new);
+set(gca, 'XTick', jIndex_new(Oj), 'XTickLabel', jLabels_new(jIndex_new(Oj)))
+set(gca, 'YTick', jIndex_new(Oj), 'YTickLabel', jLabels_new(jIndex_new(Oj)))
+title('Improved covariance matrix')
 
-set(gca, 'XTick', [], 'XTickLabel', []);
-set(gca, 'YTick', [], 'YTickLabel', []);
-setLabels('fx',  0*NB+1 : 6 :  6*NB, 'X')
-setLabels('fx',  0*NB+1 : 6 :  6*NB, 'Y')
-setLabels('q',   6*NB+1            , 'X')
-setLabels('q',   6*NB+1            , 'Y')
-setLabels('a',   7*NB+1 : 6 : 13*NB, 'X')
-setLabels('a',   7*NB+1 : 6 : 13*NB, 'Y')
-setLabels('fB', 13*NB+1 : 6 : 19*NB, 'X')
-setLabels('fB', 13*NB+1 : 6 : 19*NB, 'Y')
-setLabels('f',  19*NB+1 : 6 : 25*NB, 'X', 1)
-setLabels('f',  19*NB+1 : 6 : 25*NB, 'Y', 1)
-setLabels('t',  25*NB+1            , 'X')
-setLabels('t',  25*NB+1            , 'Y')
-
-% figure
-% subplot(211)
-% imagesc(S([myMAP.jF(end:-1:1, 1)], [myMAP.jF(end:-1:1, 1)]))
-% colorbar
-% caxis(rangeF);
-% subplot(212)
-% imagesc(S([myMAP.ja], [myMAP.ja]))
-% colorbar
-% caxis(rangeA);
-
-
-
-
+figure
+imagesc(Sd1(P, P) - Sd2(P, P)) % Sd2(Kinv(J), Kinv(J)) == S(J,J)
+colorbar
+caxis(rangeS);
+jIndex_new  = Pinv(jIndex);
+jLabels_new = jLabels(P);
+[~, Oj] = sort(jIndex_new);
+set(gca, 'XTick', jIndex_new(Oj), 'XTickLabel', jLabels_new(jIndex_new(Oj)))
+set(gca, 'YTick', jIndex_new(Oj), 'YTickLabel', jLabels_new(jIndex_new(Oj)))
+title('Difference between initial and improved covariance matrix')
 
 
