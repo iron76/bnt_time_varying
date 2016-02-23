9ed0388d677e8823f31fba4d9c4fb77f48e90b59
Claudia Latella <claudia.latella@libero.it>
2016-02-23 12:27:25 +0100

    fixed spatial inertia

--- experiments/humanFixedBase/helperFunctions/createSpatialInertia.m
+++ experiments/humanFixedBase/helperFunctions/createSpatialInertia.m
@@ -1,20 +1,21 @@
 
-function [I_O] = createSpatialInertia (I_c, m, c)
+function [I_O] = createSpatialInertia (I_cBar, m, c)
 
 
 %CREATESPATIALINERTIA compute spatial inertia in Featherstone-like
 %notatione (see Featherstone(2008),Rigid Body Dynamics Algorithms (2008), pg22).
 %
 %     I_0  spatial inertia of the rigid body with respect to the point O;
-%     I_c  spatial inertia of the rigid body with respect to the center of
+
+%     I_cBar   inertia of the rigid body with respect to the center of
 %          mass C;
 %     m    mass of the rigid body;
 %     c    position of point 0 (the same one in URDF
 %          <inertial> section).
 
 
-I_O = [ I_c + m*skew(c)*(skew(c))'  m*skew(c);
-              m*(skew(c))'          m*eye(3) ];
+I_O = [ I_cBar + m*skew(c)*(skew(c))'   m*skew(c);
+              m*(skew(c))'              m*eye(3) ];
 
 
 
